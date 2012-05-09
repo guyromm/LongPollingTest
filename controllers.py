0fc6eba699bab5daf9a43df09a76333d5519619f
Guy Romm <guyromm@gmail.com>
2012-05-09 23:58:07 +0300

    #270: a self destruction greenlet, and a "security" system for allowed access via decorators.

--- controllers.py
+++ controllers.py
@@ -4,6 +4,7 @@ filedesc: default controller file
 '''
 from noodles.http import Response,XResponse
 import json
+import gevent
 
 def index(request):
     return Response('<h1>Hello, NoodlesFramework!</h1>')
@@ -12,46 +13,80 @@ import datetime,time
 from gevent import Greenlet,queue
 
 
-def current_time(body):
-    current = start = datetime.datetime.now()
-    end = start + datetime.timedelta(seconds=60)
 
-    while current < end:
-        current = datetime.datetime.now()
-        body.put('%s\n' % current.strftime("%Y-%m-%d %I:%M:%S"))
-        time.sleep(1)
+def action(f):
+    def action_wrapper(*args,**kw):
+        f.allowed=True
+        return f(*args,**kw)
 
-    body.put('all done\n')
-    body.put(StopIteration)
-
+    return action_wrapper
 
 
 class LongPoller(object):
+    MAX_POLL_IDLETIME=3
     #uninitialized queue
     _tosend = None
 
     stopped=False
+
+
     def increment(self):
         while not self.stopped:
             self.cnt+=1
-            time.sleep(0.2)
+            gevent.sleep(0.2)
             self.send({'number':self.cnt,'ident':self.utok})
+    def kill_greenlets(self):
+        gtk = [gl for gk,gl in self.greenlets.items()] # if gk!='wiper']
+        print 'beginning to kill %s greenlets'%(len(gtk))
+        gevent.killall(gtk,block=False)
+    def _kill(self):
+        global connections
+        self.kill_greenlets()
+        try:
+            del connections[self.utok]
+        except KeyError:
+            print 'failed to delete myself (%s). am i already dead?'%self.utok 
+
+    def _wiper(self):
+        while True:
+            if (datetime.datetime.now()-datetime.timedelta(seconds=self.MAX_POLL_IDLETIME))>(self.lastpoll):
+                print 'gotta kill %s; last polled %s ago'%(self.utok,datetime.datetime.now()-self.lastpoll)
+                self._kill()
+            gevent.sleep(self.MAX_POLL_IDLETIME/2)
     def send(self,pkg):
         self._tosend.put(pkg)
     def read(self):
         return self._tosend.get()
+    greenlets = {}
+    def spawn(self,name,cb):
+        self.greenlets[name]=Greenlet.spawn(cb)
+    @action
     def startincrementor(self,request=None):
         self.stopped=False
-        self.g = Greenlet.spawn(self.increment)
+        self.spawn('increment',self.increment)
+
         if request: return XResponse({'result':'ok','value':self.stopped})
+    def _protect_methods(self):
+        for mn in dir(self):
+            attr = getattr(self,mn)
+            if not callable(attr): continue
+            if mn.startswith('_'): continue
+            raise Exception(mn)
     def __init__(self,utok):
         self._tosend =  queue.Queue()
-        self.cnt=0
         self.utok=utok
-        self.startincrementor()
+        self.lastpoll = datetime.datetime.now()
+        self.spawn('wiper',self._wiper)
+        #self._protect_methods()
         
-    def poll(self,request):
+        #test specific
+        self.cnt=0
+        self.startincrementor()
 
+    lastpoll = None
+    @action
+    def poll(self,request):
+        #print 'poll(): greenlets: %s'%self.greenlets
         item = self.read()
         if 'ident' in item and item['ident']!=self.utok:
             raise Exception('somehow my (%s) tosend gave me %s'%(self.utok,item))
@@ -59,11 +94,15 @@ class LongPoller(object):
         item['qsize']=self._tosend.qsize()
         rsp= Response('%s\n'%json.dumps(item))
         rsp.content_type='application/json'
-        #time.sleep(1)
+        #gevent.sleep(1)
+        self.lastpoll = datetime.datetime.now()
+
         return rsp
+    @action
     def putaction(self,request):
         self.send({'content':request.params.get('c'),'ident':self.utok})
         return XResponse({'result':'ok'})
+    @action
     def stop(self,request):
         self.stopped=True
         return XResponse({'result':'ok','value':self.stopped})
@@ -73,7 +112,7 @@ def longpolling(request,conn_info):
     toks = conn_info.split('/')
     
     utok = toks[0]
-    print 'utok= %s'%utok
+    #print 'utok= %s'%utok
     if len(toks)>1:
         action = toks[1]
     else:
@@ -83,10 +122,15 @@ def longpolling(request,conn_info):
         connections[utok] = LongPoller(utok)
         print 'just instantiated a new longpoller. got %s so far.'%(len(connections))
     lp = connections[utok]
-    return getattr(lp,action)(request)
+    #we do not allow calling internal methods at all.
+    assert not action.startswith('_'),"security violation"
+    f = getattr(lp,action)
+    assert ".action_wrapper" in str(f),"%s is not allowed to be executed externally."%(action)
 
-    raise Exception(conn_info)
+    return f(request)
 
+
+#this is a demonstration of streaming
 from noodles.websocket.streaming import StreamQueue ,PartialResponse
 def streamer(request,conn_id):
     resp = PartialResponse()
@@ -98,3 +142,16 @@ def streamer(request,conn_id):
     g = Greenlet.spawn(current_time,body)
 
     return body
+
+def current_time(body):
+    current = start = datetime.datetime.now()
+    end = start + datetime.timedelta(seconds=60)
+
+    while current < end:
+        current = datetime.datetime.now()
+        body.put('%s\n' % current.strftime("%Y-%m-%d %I:%M:%S"))
+        gevent.sleep(1)
+
+    body.put('all done\n')
+    body.put(StopIteration)
+
